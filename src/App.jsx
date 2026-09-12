edc086e8961d54283f21485b2ad60ec4ab0a541e
Kanishk bhati <kanishkbhati007@gmail.com>
2026-09-12 23:37:31 +0530

    functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { X } from 'lucide-react'
 
 const App = () => {
   const [title, settitle] = useState('')
@@ -18,6 +19,10 @@ const App = () => {
     
     settitle('')
     setDetails('')
+    
+  }
+  const noteDelete=()=>{
+    const copyTask=(...task)
     
   }
 
@@ -61,11 +66,10 @@ const App = () => {
 
 
         <button
-        className=' w-full bg-white text-black px-5 py-2 rounded outline-none font-medium  active:scale-99'>
+        className=' w-full bg-white text-black px-5 py-2 rounded outline-none font-medium  active:scale-99 cursor-pointer '>
           Add notes
         </button>
       
-      {/* <img className='h-52 ' src="https://freepngimg.com/save/74899-note-notes-paper-sticky-post-it-png-file-hd/1161x1168" alt="" /> */}
       </form>
 
 
@@ -76,10 +80,13 @@ const App = () => {
         
        {Task.map(function(elem,idx){
         return(
-          <div key={idx} className="h-52 w-40 p-4  rounded-2xl bg-center bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB_c4ZFRcbahuz6SKDufjznet5FczfRyaQAkwJ7Rrcgw&s=10')]">
+          <div key={idx} className="relative flex flex-col justify-between h-52 w-40 p-4  rounded-2xl bg-center bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB_c4ZFRcbahuz6SKDufjznet5FczfRyaQAkwJ7Rrcgw&s=10')]">
+          <div>
            <h3 className=' text-black leading-tight font-bold text-xl'>{elem.title}</h3> 
            <h3 className='leading-tight text-xl mt-2 text-gray-500'>{elem.Details}</h3> 
-            </div>
+          </div> 
+          <button onClick={noteDelete}  className='hover:bg-red-600 cursor-pointer active:scale-95 rounded-full p-1 bg-red-400 text-xs font-bold '>Delete</button>
+          </div>
         )
         
        })}
