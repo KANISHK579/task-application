3cd90f31f266175a36b7b3d543947a8e5d5803b1
Kanishk bhati <kanishkbhati007@gmail.com>
2026-09-18 23:38:46 +0530

    localstorage functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { X } from 'lucide-react'
 import Notes from './components/Notes'
 import Form from './components/Form'
 
 const App = () => {
+  const [Task, setTask] = useState(()=>{
+    const localdata =localStorage.getItem('Stored_notes')
+
+    return localdata? JSON.parse(localdata) :[];
+  });
+
+
+  useEffect(() => {
+    localStorage.setItem('Stored_notes',JSON.stringify(Task))
+  
+  }, [Task])
+
+
+  
+  
   const [title, settitle] = useState('')
   const [Details, setDetails] = useState('')
 
-  const [Task, setTask] = useState([])
+  
   
   const submit_handler=(e)=>{
     e.preventDefault()
@@ -33,14 +48,14 @@ const App = () => {
 
   
   return (
-    <div className='h-screen lg:flex bg-black text-white justify-between  ' >
+    <div className='h-screen lg:flex max-sm:flex max-sm:flex-col  bg-black text-white justify-between  ' >
       <Form  title={title}
         settitle={settitle}
         Details={Details}
         setDetails={setDetails}
         submit_handler={submit_handler} />
 
-      <Notes   Task={Task}
+      <Notes  Task={Task}
         noteDeleted={noteDeleted} />
       
     </div>
--- src/components/utility/NoteCard.jsx
+++ src/components/utility/NoteCard.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const NoteCard = ({idx,elem,noteDeleted}) => {
   return (
-     <div key={idx} className="relative flex flex-col  justify-between h-52 w-40 p-4  rounded-2xl bg-center bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB_c4ZFRcbahuz6SKDufjznet5FczfRyaQAkwJ7Rrcgw&s=10')]">
+     <div key={idx} onDoubleClick={()=>{
+      
+     }} className="relative flex flex-col  justify-between h-52 w-40 p-4  rounded-2xl bg-center bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB_c4ZFRcbahuz6SKDufjznet5FczfRyaQAkwJ7Rrcgw&s=10')] ">
           <div className='min-w-0' >
           
               <p
