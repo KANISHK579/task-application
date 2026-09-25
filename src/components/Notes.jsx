1c979a594b61b1bb44034115eaa97fb77ca90a9b
Kanishk bhati <kanishkbhati007@gmail.com>
2026-09-25 23:49:13 +0530

    media Query feature added

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import NoteCard from './utility/NoteCard'
 
 const Notes = ({Task,noteDeleted}) => {
+
+  if (!Task || Task.length===0){
+    return null;
+  }
   return (
-     <div className=' p-10 bg-gray-800 lg:w-1/2 lg:border-l-3 '>
+     <div className=' p-10 bg-gray-800 lg:w-1/2 lg:border-l-3  '>
       <h1 className='text-3xl font-bold'>Recent notes</h1>
       <div className='flex flex-wrap gap-4 overflow-auto   mt-5 h-full max-sm:justify-center items-center'>
         
--- src/components/utility/NoteCard.jsx
+++ src/components/utility/NoteCard.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const NoteCard = ({idx,elem,noteDeleted}) => {
   return (
-     <div key={idx} onDoubleClick={()=>{
-      
-     }} className="relative flex flex-col  justify-between h-52 w-40 p-4  rounded-2xl bg-center bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB_c4ZFRcbahuz6SKDufjznet5FczfRyaQAkwJ7Rrcgw&s=10')] ">
-          <div className='min-w-0' >
+     <div key={idx} 
+          
+     className="relative flex flex-col  justify-between h-52 w-40 p-4  rounded-2xl bg-center bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB_c4ZFRcbahuz6SKDufjznet5FczfRyaQAkwJ7Rrcgw&s=10')] ">
+          <div className='' >
           
               <p
                 className=' h-auto text-black leading-tight font-bold text-lg  truncate '>{elem.title}</p>
@@ -19,7 +19,7 @@ const NoteCard = ({idx,elem,noteDeleted}) => {
           </div> 
           <button onClick={()=>{
             noteDeleted(idx)
-          }}  className='hover:bg-red-600 cursor-pointer active:scale-95 rounded-full p-1 bg-red-400 text-xs font-bold mt-1'>Delete</button>
+          }}  className='hover:bg-red-600 cursor-pointer active:scale-95 rounded-full p-1 max-sm:bg-red-600 bg-red-400 text-xs font-bold mt-1'>Delete</button>
           </div>
   )
 }
